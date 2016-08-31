9d1f8032694b65bcefd8b4c32540f965042d39e7
SkiGeek82 <justin.r.dietz@gmail.com>
2016-08-30 21:42:32 -0700

    Basic Filtering Functionality added as well as changes to show image

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,53 @@
-// JavaScript source code
 'use strict';
 
 var rundata = {
-    "runs": [{
-        "name": "Lucky Shot",
-        "description": "The easiest way down from the top of the Gondola or Raineir express offers a mixture of steep slopes seperated by mild runouts. With plenty of cat tracks to bypass the steeper slopes this is defnitely your easiest way down",
-        "difficulty": "blue",
-        "surface":"Groomed"},
-            {
-        "name": "Snorting Elk",
-        "description": "Offering a number of routes from a steep groomed slope powder. This run varies on the day but is worth checking out.",
-        "difficulty": "black",
-        "surface": "Mixed"
-            }
+    "runs": [
+        {
+            "id": 1,
+            "name": "Lucky Shot",
+            "description": "The easiest way down from the top of the Gondola or Raineir express offers a mixture of steep slopes seperated by mild runouts. With plenty of cat tracks to bypass the steeper slopes this is defnitely your easiest way down",
+            "difficulty": "blue",
+            "surface": "Groomed",
+            "image": "./images/runs/luckyshot.jpg"
+        },
+        {
+            "id": 2,
+            "name": "Snorting Elk",
+            "description": "Offering a number of routes from a steep groomed slope to powder. This run varies on the day but is worth checking out.",
+            "difficulty": "black",
+            "surface": "Mixed",
+            "image": "./images/runs/luckyshot.jpg"
+        },
+        {
+            "id": 3,
+            "name": "Brain Damage",
+            "description": "A hike from the throne saddle around the back and up to the top of Silver King leads you to find a number of entrances into A basin the main entrance from the peak is Brain Damage.",
+            "difficulty": "black",
+            "surface": "Mixed",
+            "image": "./images/runs/luckyshot.jpg"
+        }
     ]
 };
 
 
-/* We probably don't need a default filter we will just clear the current filter*/
-var defaultFilter = { "difficulty": ["green", "blue", "black", "doubleblack"] }
+// We probably don't need a default filter we will just clear the current filter
+// var defaultFilter = { "difficulty": ["green", "blue", "black", "doubleblack"] }
+
+var btnFilter = document.getElementById("applyfilter");
+btnFilter.addEventListener("click", filterData);
 
 window.onload = function () {
-    formatResults(rundata, "results");
+    formatResults(rundata.runs, "results");
 }
 
+function filterData(filters) {
+    var text = document.getElementById("filterText").value;
+    formatResults(rundata.runs.filter(filterByText(text)), "results");
+}
+
+//find all runs where name includes a specified value
+function filterByText(value) {
+    return function (element) {
+        return element.name.toLowerCase().includes(value.toLowerCase()) || element.description.toLowerCase().includes(value.toLowerCase());
+    }
+}
--- scripts/template-results.js
+++ scripts/template-results.js
@@ -1,4 +1,4 @@
-/* I am seperating this out as it has UI display aspects*/
+/* I am separating this out as it has UI display aspects*/
 'use strict';
 
 /*  There are better ways to do this with some additional frameworks 
@@ -9,17 +9,29 @@
 
 //takes a run object and returns the html needed to render the result
 function formatResult(run) {
-    var output = '<div class="result"><div class="result-left"><a href="#"><img class="result-obj" src="..." alt="..."></a></div> \
-    <div class="result-body"><h4 class="result-heading">' + run.name + '</h4>' + run.description + '</div></div>';
+    var output = '  <div class="result"> \
+                        <div class="result-left"> \
+                            <a href="#"> \
+                                <img class="result-obj" src="' + run.image + '" alt="' + run.name + '"> \
+                            </a> \
+                        </div> \
+                        <div class="result-body"> \
+                            <h4 class="result-heading">' + run.name + '</h4>' + run.description + ' \
+                        </div> \
+                    </div>';
     return output;
 }
 
+// change this so that it takes an array and formats and the specific array object
+// in the object is not needed to be known and so it works with filters
 function formatResults(obj, target) {
     var results = "";
-    for (var i = 0, len = obj.runs.length; i<len; i++)
+    for (var i = 0, len = obj.length; i<len; i++)
     {
-        results += formatResult(obj.runs[i]);
+        results += formatResult(obj[i]);
     }
     //formatResult(run)
     document.getElementById(target).innerHTML = results;
 }
+
+
