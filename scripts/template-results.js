178f2764ace36e03d8a4f892b6f85b617164ebb0
SkiGeek82 <justin.r.dietz@gmail.com>
2016-09-04 14:04:53 -0700

    JSLinting with  Google JavaScript Style Standards

--- scripts/template-results.js
+++ scripts/template-results.js
@@ -1,42 +1,40 @@
-/* I am separating this out as it has UI display aspects*/
 'use strict';
+/* I am separating this out as it has UI display aspects*/
 
-/*  There are better ways to do this with some additional frameworks 
+/*  There are better ways to do this with some additional frameworks
     but with pure javascript and no jquery, mustache, angular this is
-    the approach I am going to take. It honestly reminds me of code I 
+    the approach I am going to take. It honestly reminds me of code I
     wrote in 1999.
     */
 
-//takes a run object and returns the html needed to render the result
+// takes a run object and returns the html needed to render the result
 function formatResult(run) {
-    var output = '  <div class="result">' +
+  var output = '  <div class="result">' +
                         '<div class="result-left">' +
                             '<a href="#">' +
                                 '<img class="result-obj" src="' + run.image + '" alt="' + run.name + '">' +
                             '</a> ' +
                         '</div> ' +
                         '<div class="result-body"> ' +
-                            
+
                             '<h4 class="result-heading">' + run.name + '</h4>' +
                             '<h6 class="result-subheading">' + run.resort + '</h6>' + run.description + ' ' +
                         '</div> ' +
-                        '<div class="result-menu">' + 
+                        '<div class="result-menu">' +
                             '<div class="diff-rating"><img  src="./images/' + run.difficulty + '.svg" /></div>' +
                         '</div>' +
                     '</div>';
-    return output;
+  return output;
 }
 
 // change this so that it takes an array and formats and the specific array object
 // in the object is not needed to be known and so it works with filters
 function formatResults(obj, target) {
-    var results = "";
-    for (var i = 0, len = obj.length; i<len; i++)
-    {
-        results += formatResult(obj[i]);
-    }
-    //formatResult(run)
-    document.getElementById(target).innerHTML = results;
+  var results = '';
+  for (var i = 0, len = obj.length; i < len; i++) {
+    results += formatResult(obj[i]);
+  }
+    // formatResult(run)
+  document.getElementById(target).innerHTML = results;
 }
-
 
